4a6852778dd9fa7866e6e2350932616212a4c7fa
Toby <5722599@gmail.com>
2022-05-03 18:45:26 -0400

    setup styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: red;
+  width: 40px;
+`
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Button>test</Button>
     </div>
   );
 }
