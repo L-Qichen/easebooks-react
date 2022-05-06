9655eb5bf30b754fa4b041b9116eb6b6e1f95c8c
Toby <5722599@gmail.com>
2022-05-06 09:57:22 -0400

    setup svg-sprite-loader and svo-loader on webpack config file. Done nav bar icons.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import React from "react";
+/**这里使用 require 代替 import 是因为
+ * TreeShaking 不适用于 require 
+ * 可以避免使用 console.log(x);
+ **/
+require('icons/records.svg');
+require('icons/chart.svg');
+require('icons/bookkeeping.svg');
 
 const NavBox = styled.div`
 line-height: 24px;
@@ -10,7 +17,14 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   >li {
     width: 33.3333333%;
     text-align: center;
-    padding: 16px;
+    padding: 6px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon {
+      width: 24px;
+      height: 24px;
+    }
   }
 }
 `;
@@ -20,12 +34,21 @@ const Nav = () => {
     <NavBox>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#records" />
+          </svg>
           <Link to="/records">Records</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#bookkeeping" />
+          </svg>
           <Link to="/money">Money</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/stats">Stats</Link>
         </li>
       </ul>
