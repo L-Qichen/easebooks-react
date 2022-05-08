6d54668a1c2420053b8ada31d364a83b26ea7d7a
Toby <5722599@gmail.com>
2022-05-08 15:16:00 -0400

    The UI of TagsSection part of Money.tsx done.

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,25 @@ import Records from 'views/Records';
 import Money from 'views/Money';
 import Stats from 'views/Stats';
 import NoMatch from 'views/NoMatch';
+import styled from 'styled-components';
 
 function App() {
+  const AppWrapper = styled.div`
+    color: #333;
+  `
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/records" element={<Records />} />
-        <Route path="/money" element={<Money />} />
-        <Route path="/stats" element={<Stats />} />
-        <Route path="/" element={<Navigate replace to="/money" />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route path="/records" element={<Records />} />
+          <Route path="/money" element={<Money />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/" element={<Navigate replace to="/money" />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,93 @@
 import React from "react";
 import Layout from "components/layout";
+import styled from "styled-components";
 
 function Money() {
+  const TagsSection = styled.section`
+    background: #FFFFFF;
+    border: 1px solid red;
+    padding: 12px 16px;
+    > ol {
+      margin: 0px -12px;
+      > li {
+        background: #D9D9D9;
+        border-radius: 18px;
+        display: inline-block;
+        padding: 3px 18px;
+        margin: 8px 12px;
+      }
+    }
+    > button {
+      background: none;
+      border: none;
+      padding: 2px 4px;
+      border-bottom: 1px solid #333;
+      color: #666;
+      margin-top: 8px;
+    }
+  `
+
+  const NotesSection = styled.section`
+    
+  `
+
+  const CategorySection = styled.section`
+    
+  `
+
+  const NumberPadSection = styled.section`
+  
+  `
+
   return (
     <Layout>
-      <h2>Money page</h2>
+      <TagsSection>
+        <ol>
+          <li>Food</li>
+          <li>Clothing</li>
+          <li>Housing</li>
+          <li>transportation</li>
+        </ol>
+        <button>add new tag</button>
+      </TagsSection>
+
+      <NotesSection>
+        <label>
+          <span>
+            Commit
+          </span>
+          <input type="text" />
+        </label>
+      </NotesSection>
+
+      <CategorySection>
+        <ul>
+          <div>income</div>
+          <div>outcome</div>
+        </ul>
+      </CategorySection>
+
+      <NumberPadSection>
+        <div>
+          100
+        </div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>Del.</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>C</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </NumberPadSection>
     </Layout>
   );
 }
