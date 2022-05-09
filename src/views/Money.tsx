ee2845e4d6e06ade2f3ecc39501e80de090292b2
Toby <5722599@gmail.com>
2022-05-08 20:20:56 -0400

    The UI of NumberPadSection part of Money.tsx done.

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,6 @@ import styled from "styled-components";
 function Money() {
   const TagsSection = styled.section`
     background: #FFFFFF;
-    border: 1px solid red;
     padding: 12px 16px;
     > ol {
       margin: 0px -12px;
@@ -46,11 +45,86 @@ function Money() {
   `
 
   const CategorySection = styled.section`
-    
+    font-size: 24px;
+    > ul {
+      display: flex;
+      background: #C4C4C4;
+      > li {
+        width: 50%;
+        text-align: center;
+        padding: 16px 0px;
+        position: relative;
+        &.selected::after {
+          content: '';
+          display: block;
+          height: 4px;
+          border-radius: 32px;
+          background: #333;
+          position: absolute;
+          width: 100%;
+          bottom: 0;
+          left: 0;
+        }
+      }
+    }
   `
 
   const NumberPadSection = styled.section`
-  
+    display: flex;
+    flex-direction: column;
+    > .output {
+      background: white;
+      font-size: 36px;
+      line-height: 72px;
+      text-align: right;
+      padding: 0px 16px;
+      box-shadow: inset 0px -5px 5px -5px rgba(0,0,0,0.25),
+                  inset 0px 5px 5px -5px rgba(0,0,0,0.25);
+    }
+    > .pad {
+      > button {
+        font-size: 18px;
+        border: none;
+        float: left;
+        width: 25%;
+        height: 64px;
+        &.ok {
+          float: right;
+          height: 128px;
+        }
+        &.zero {
+          width: 50%;
+        }
+        &:nth-child(1) {
+          background: #f2f2f2;
+        }
+        &:nth-child(2),
+        &:nth-child(5) {
+          background: #E0E0E0;
+        }
+        &:nth-child(3),
+        &:nth-child(6),
+        &:nth-child(9) {
+          background: #D3D3D3;
+        }
+        &:nth-child(4),
+        &:nth-child(7),
+        &:nth-child(10) {
+          background: #C1C1C1;
+        }
+        &:nth-child(8),
+        &:nth-child(11),
+        &:nth-child(13) {
+          background: #B8B8B8;
+        }
+        &:nth-child(12) {
+          background: #9A9A9A;
+        }
+        &:nth-child(14) {
+          background: #A9A9A9;
+        }
+      }
+    }
   `
 
   return (
@@ -76,16 +150,16 @@ function Money() {
 
       <CategorySection>
         <ul>
-          <div>income</div>
-          <div>outcome</div>
+          <li className="selected">Incomes</li>
+          <li>Expenses</li>
         </ul>
       </CategorySection>
 
       <NumberPadSection>
-        <div>
+        <div className="output">
           100
         </div>
-        <div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -97,8 +171,8 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
