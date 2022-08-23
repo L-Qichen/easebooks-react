7ef92fb9633c4f413d33860fbc807805f8412625
Toby <5722599@gmail.com>
2022-08-23 15:54:23 -0400

    Display bookkeeping records by date.

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useUpdate } from "./useUpdate";
 
-type RecordInfo = {
+export type RecordInfo = {
   tagIds: number[];
   note: string;
   category: '+' | '-';
--- src/views/Records.tsx
+++ src/views/Records.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import Layout from "components/layout";
 import { CategorySection } from "./Money/CategorySection";
-import { useRecords } from "hooks/useRecords";
+import { RecordInfo, useRecords } from "hooks/useRecords";
 import day from 'dayjs'
 import styled from "styled-components";
 import { useTags } from "hooks/useTags";
+import "core-js";
 
 function Records() {
   const Item = styled.div`
@@ -14,44 +15,73 @@ function Records() {
     font-size: 18px;
     line-height: 20px;
     padding: 10px 16px;
-    > .tags span {
-      padding-right: 6px;
-    }
     > .note {
       margin-right: auto;
-      margin-left: 10px;
+      margin-left: 16px;
       color: #aaa;
     }
   `;
+
+  const Header = styled.h3`
+    font-size: 18px;
+    line-height: 20px;
+    padding: 10px 16px;
+  `;
+
   const [category, setCategory] = useState<'-' | '+'>('-');
   const { records } = useRecords();
   const { getName } = useTags();
+  const selectedRecords = records.filter(record => record.category === category);
+  const hash: { [K: string]: RecordInfo[] } = {};
+
+  selectedRecords.forEach(record => {
+    const key = day(record.createdAt).format('YYYY-MM-DD');
+    if (!(key in hash)) {
+      hash[key] = [];
+    };
+    hash[key].push(record);
+  });
+
+  const array = Object.entries(hash).sort((a, b) => {
+    if (a[0] === b[0]) { return 0 };
+    if (a[0] > b[0]) { return -1 };
+    if (a[0] < b[0]) { return 1 };
+    return 0;
+  });
   return (
     <Layout>
       <CategorySection value={category}
         onChange={value => { setCategory(value) }}
       />
-      <div>
-        {records.map(record => {
-          return (
-            <Item>
-              <>
-                <div className="tags">
-                  {record.tagIds.map(tagId => {
-                    return <span>{getName(tagId)}</span>
-                  })}
-                </div>
-              </>
-              {record.note && <div className="note">
-                {record.note}
-              </div>}
-              <div className="amount">
-                ${record.amount}
-              </div>
-              {/* {day(record.createdAt).format('YYYY-MM-DD')} */}
-            </Item>);
-        })}
-      </div>
+      {array.map(([date, records]) => {
+        return (
+          <div key={date}>
+            <Header>
+              {date}
+            </Header>
+            <div>
+              {records.map(record => {
+                return (
+                  <Item key={record.createdAt}>
+                    <div className="tags">
+                      {record.tagIds.map(tagId => {
+                        return <span key={tagId}>{getName(tagId)}</span>
+                      })
+                        .reduce((result, span, index, array) =>
+                          result.concat(index < array.length - 1 ? [span, ', '] : [span]), [] as ReactNode[])}
+                    </div>
+                    {record.note && <div className="note">
+                      {record.note}
+                    </div>}
+                    <div className="amount">
+                      ${record.amount}
+                    </div>
+                  </Item>);
+              })}
+            </div>
+          </div>
+        )
+      })}
     </Layout >
   );
 }
